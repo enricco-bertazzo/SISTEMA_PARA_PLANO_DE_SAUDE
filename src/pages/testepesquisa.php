d7ae7fc1647cdb3cf82038ad411b7d0484579d76
enricco-bertazzo <enriccowhz@gmail.com>
2021-10-04 07:27:17 -0300

    pesquisa feita

---
+++ src/pages/testepesquisa.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form method="POST" action="../php/pesquisa.php">
+            <input name="cpf_query" placeholder="cpf" type="text">
+            <input name="cnpj_query" placeholder="cnpj" type="text">
+            <input name="crm_query" placeholder="crm" type="text">
+            <button type="submit" class="btn btn-primary btn-block" style="width:100%">Vincular</button>
+        </form>
+    </body>
+</html>
--- src/php/pesquisa.php
+++ src/php/pesquisa.php
@@ -1,14 +1,50 @@
 <?php
-$cpf_query = '000.000.000-00';
-$senha = '123';
-$nome = 'enricco';
-$endereco = 'portugal';
-$telefone = '991102311';
-$email = 'enricco@gmail.com';
-$genero = 'neutro';
+
+$cpf_query = $_POST['cpf_query'];
+$crm_query = $_POST['crm_query'];
+$cnpj_query = $_POST['cnpj_query'];
 
 $xml = simplexml_load_file('../xml/cadastro_pacientes.xml');
 
-
+if(isset($cpf_query)){
+    foreach($xml -> paciente as $paciente){
+        if ($paciente -> cpf == $cpf_query){
+            echo $paciente -> cpf;
+            echo $paciente -> senha;
+            echo $paciente -> nome;
+            echo $paciente -> endereco;
+            echo $paciente -> telefone;
+            echo $paciente -> email;
+            echo $paciente -> genero;
+            exit;
+        }
+    }
+} elseif(isset($crm_query)){
+    foreach($xml -> medico as $medico){
+        if ($medico -> crm == $crm_query){
+            echo $medico -> crm;
+            echo $medico -> senha;
+            echo $medico -> nome;
+            echo $medico -> endereco;
+            echo $medico -> telefone;
+            echo $medico -> email;
+            echo $medico -> especialidade;
+            exit;
+        }
+    }
+} elseif(isset($cnpj_query)){
+    foreach($xml -> laboratorio as $laboratorio){
+        if ($laboratorio -> cnpj == $cnpj_query){
+            echo $laboratorio -> cnpj;
+            echo $laboratorio -> senha;
+            echo $laboratorio -> nome;
+            echo $laboratorio -> endereco;
+            echo $laboratorio -> telefone;
+            echo $laboratorio -> email;
+            echo $laboratorio -> exame;
+            exit;
+        }
+    }
+}
 
 ?>
